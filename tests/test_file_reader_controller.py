0a977384f6b34f0d01ee6430f4db31eeabb5dc3f
Volodin Alexandr Sergeevich <endocrinologist@ya.ru>
2025-11-09 07:30:01 +0400

    FileReaderController

---
+++ tests/test_file_reader_controller.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+from onco_cola_utils import logsuc
+from src.onco_cola_utils import log, logerr
+from src.onco_cola_utils.file_reader_controller import FileReaderController
+
+
+print = log
+
+
+def get_curent_path() -> Path:
+    return Path(__file__).resolve().parent
+
+
+def test_file_reader_controller_initialization():
+    """Проверка инициализации ReaderController"""
+    file_path: Path = get_curent_path() / "text.txt"
+
+    write_data: str = "some_data"
+
+    print(f"{file_path=}")
+    if not file_path.exists():
+        result = FileReaderController.save_text(file_path, write_data)
+        if not result:
+            logerr("❌ ЗАПИСЬ В ФАЙЛ НЕ ПРОИЗОШЛА")
+        else:
+            logsuc("✅ ДАННЫЕ ЗАПИСАНЫ В ФАЙЛ")
+
+    data: str = FileReaderController.read_text(file_path)
+    print(f"{data=}")
+
+    assert data == write_data
+
+    print("✅ FileReaderController инициализирован корректно")
+
+
+if __name__ == "__main__":
+    # Запуск тестов напрямую
+    test_file_reader_controller_initialization()
