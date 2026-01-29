0bbdbeb4ebd2a27b5a0f7390db03ab16ebd5dd5b
Volodin Alexandr Sergeevich <endocrinologist@ya.ru>
2026-01-29 15:11:46 +0400

    добавлено тестирование для логгера

---
+++ tests/test_logger.py
@@ -0,0 +1,9 @@
+from onco_cola_utils import loginf, logsuc, logwarn
+from src.onco_cola_utils import log, logerr
+
+
+log("DEBUG")
+loginf("INFO")
+logwarn("WARNING")
+logerr("ERROR")
+logsuc("SUCCESS")
